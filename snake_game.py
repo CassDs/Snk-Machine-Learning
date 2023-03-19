cbca11403c6bf4e93591a811fc383d03446e8ca2
L0k1 <caiocryptotab01@gmail.com>
2023-03-19 19:46:32 -0300

    Adionando novas funcionalidades

--- snake_game.py
+++ snake_game.py
@@ -7,6 +7,8 @@ SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 SNAKE_SIZE = 20
 SNAKE_SPEED = 15
+LEVEL_UP_SCORE = 5
+
 
 # Cores
 WHITE = (255, 255, 255)
@@ -70,17 +72,45 @@ def draw_objects(screen, snake, food):
 
     pygame.draw.rect(screen, RED, pygame.Rect(food[0], food[1], SNAKE_SIZE, SNAKE_SIZE))
 
+#Configuração de dificuldade do jogo
+
+def draw_level(screen, level):
+    rounded_level = round(level)  
+    font = pygame.font.Font(None, 24)
+    text = font.render(f"Nível: {rounded_level}", True, (0, 0, 0))  
+    screen.blit(text, (SCREEN_WIDTH - 100, 10))
+
+#Ler a pontuação mais alta
+def read_high_score():
+    try:
+        with open("high_score.txt", "r") as file:
+            high_score = int(file.read())
+    except (FileNotFoundError, ValueError):
+        high_score = 0
+    return high_score
+
+#Salva a pontuação mais alta
+def save_high_score(high_score):
+    with open("high_score.txt", "w") as file:
+        file.write(str(high_score))
+
+#Exibe pontuação mais alta em tela
+def draw_high_score(screen, high_score):
+    font = pygame.font.Font(None, 24)
+    text = font.render(f"Pontuação mais alta: {high_score}", True, (0, 0, 0))
+    screen.blit(text, (SCREEN_WIDTH - 250, 10))
+
+
 #Configurações do jogo
 def main():
-
-    # Crie a janela do jogo e defina o título
+    # Configura a tela do jogo e define o título
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Snake Game")
 
-    # Controle a velocidade do jogo
+    # Configura o relógio para controlar a velocidade do jogo
     clock = pygame.time.Clock()
 
-    # Adicione esta parte para exibir a tela de início
+    # Exibe a tela de início e aguarda o jogador pressionar uma tecla
     draw_start_screen(screen)
     pygame.display.flip()
     waiting = True
@@ -92,20 +122,25 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 waiting = False
 
-    # Inicialize a cobrinha e a comida
+    # Inicializa a cobra e a comida
     snake = create_snake()
     food = create_food(snake)
 
-    # Variáveis para armazenar a direção atual e a próxima direção da cobrinha
+    # Define a direção inicial da cobra e a próxima direção
     direction = pygame.K_RIGHT
     next_direction = direction
 
-    # Inicialize a pontuação
+    # Inicializa a pontuação e o nível do jogo
     score = 0
+    level = 1
 
+    # Lê a pontuação mais alta salva
+    high_score = read_high_score()
+
+    # Inicia o loop principal do jogo
     running = True
     while running:
-        # Verifique se algum evento ocorreu, como pressionar uma tecla
+        # Verifica se algum evento ocorreu, como pressionar uma tecla
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -113,7 +148,7 @@ def main():
                 if event.key in (pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT):
                     next_direction = event.key
 
-        # Verifique se a próxima direção é válida e atualize a direção atual
+        # Atualiza a direção da cobra, se a próxima direção for válida
         if (
             next_direction == pygame.K_UP and direction != pygame.K_DOWN
             or next_direction == pygame.K_DOWN and direction != pygame.K_UP
@@ -122,7 +157,7 @@ def main():
         ):
             direction = next_direction
 
-        # Mova a cobrinha na direção atual
+        # Atualiza a posição da cobra com base na direção atual
         dx, dy = 0, 0
         if direction == pygame.K_UP:
             dy = -SNAKE_SIZE
@@ -135,52 +170,62 @@ def main():
 
         new_head = ((snake[0][0] + dx) % SCREEN_WIDTH, (snake[0][1] + dy) % SCREEN_HEIGHT)
 
-        # Verifique se a cobrinha colidiu consigo mesma
-        if (
-            
-            new_head in snake
-
-        ):
+        # Verifica se a cobra colidiu consigo mesma e encerra o jogo, se necessário
+        if new_head in snake:
             running = False
 
-            # Exiba a tela de "Game Over"
-            draw_game_over_screen(screen, score)
-            pygame.display.flip()
-
-            # Aguarde o jogador pressionar uma tecla
-            waiting = True
-            while waiting:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        return
-                    elif event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_r:
-                            waiting = False
-                            main()  # Reinicie o jogo chamando a função `main()` novamente
-                        elif event.key == pygame.K_ESCAPE:
-                            pygame.quit()
-                            return
-
-        # Atualize a posição da cobrinha
+        # Insere a nova posição da cabeça da cobra
         snake.insert(0, new_head)
 
-        # Verifique se a cobrinha comeu a comida e atualize sua posição
+        # Verifica se a cobra comeu a comida e atualiza sua posição
         if new_head == food:
             food = create_food(snake)
-            score += 1  # Atualize a pontuação
+            score += 1  # Atualiza a pontuação
+
+            # Verifica se o jogador atingiu o próximo nível
+            if score % LEVEL_UP_SCORE == 0:
+                level += 0.1
         else:
+            # Remove o último segmento da cobra, caso não tenha comido a comida
             snake.pop()
 
-        # Desenhe a cobrinha e a comida na tela
+        # Desenha a cobra, a comida, a pontuação e o nível na tela
         draw_objects(screen, snake, food)
-        draw_score(screen, score) 
+        draw_score(screen, score)
+        draw_level(screen, level)
         pygame.display.flip()
 
-        # Controle a velocidade do jogo
-        clock.tick(SNAKE_SPEED)
+        # Atualiza a velocidade da cobra com base no nível atual
+        clock.tick(SNAKE_SPEED * level)
+
+    # O jogo terminou, então verifique se a pontuação atual é maior que a pontuação mais alta e salve, se necessário
+    if score > high_score:
+        high_score = score
+        save_high_score(high_score)
+
+    # Exibe a tela de "Game Over" e aguarda o jogador pressionar uma tecla
+    draw_game_over_screen(screen, score)
+    pygame.display.flip()
+
+    draw_high_score(screen, high_score)
+    pygame.display.flip()
+
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    waiting = False
+                    main()  # Reinicie o jogo chamando a função `main()` novamente
+                elif event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    return
 
     pygame.quit()
 
 if __name__ == "__main__":
     main()
+
